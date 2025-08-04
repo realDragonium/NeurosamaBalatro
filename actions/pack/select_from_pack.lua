7cd7dd8e4b41923282619f81f1a4839d02c98c76
Dragonium <drago@dragonium.nl>
2025-08-04 18:00:10 +0200

    Update selecto from pack action, to be more straight foward when only 1 card is allowed

--- actions/pack/select_from_pack.lua
+++ actions/pack/select_from_pack.lua
@@ -3,13 +3,27 @@
 local CardUtils = SMODS.load_file("utils/card_utils.lua")()
 
 local function select_from_pack_executor(params)
-    local card_indices = params.card_indices
-    if not card_indices or #card_indices == 0 then
-        return false, "card_indices is required"
-    end
-
+    local max_cards = G.GAME.pack_choices or 1
+    
     if not G.pack_cards then
         return false, "No pack cards available"
+    end
+    
+    -- Handle both single index and array of indices
+    local card_indices = {}
+    if max_cards == 1 then
+        -- Single card selection
+        local card_index = params.card_index
+        if not card_index then
+            return false, "card_index is required"
+        end
+        card_indices = {card_index}
+    else
+        -- Multiple card selection
+        card_indices = params.card_indices
+        if not card_indices or #card_indices == 0 then
+            return false, "card_indices is required"
+        end
     end
 
     local cards_to_select = {}
@@ -46,31 +60,56 @@ local function create_select_from_pack_action()
     -- Only available when pack cards are present
     if G.pack_cards and G.pack_cards.cards then
         local max_cards = G.GAME.pack_choices
+        local total_cards = #G.pack_cards.cards
         if max_cards > 0 then
-            return {
-                name = "select_from_pack",
-                definition = {
+            if max_cards == 1 then
+                -- Single card selection - use simple integer parameter
+                return {
                     name = "select_from_pack",
-                    description = "Select cards from an opened pack (1-" .. max_cards .. ")",
-                    schema = {
-                        type = "object",
-                        properties = {
-                            card_indices = {
-                                type = "array",
-                                items = {
+                    definition = {
+                        name = "select_from_pack",
+                        description = "Select 1 card from the opened pack (1-" .. total_cards .. ")",
+                        schema = {
+                            type = "object",
+                            properties = {
+                                card_index = {
                                     type = "integer",
                                     minimum = 1,
-                                    maximum = max_cards
-                                },
-                                minLength = 1,
-                                maxLength = max_cards,
-                            }
-                        },
-                        required = {"card_indices"}
-                    }
-                },
-                executor = select_from_pack_executor
-            }
+                                    maximum = total_cards
+                                }
+                            },
+                            required = {"card_index"}
+                        }
+                    },
+                    executor = select_from_pack_executor
+                }
+            else
+                -- Multiple card selection - use array parameter
+                return {
+                    name = "select_from_pack",
+                    definition = {
+                        name = "select_from_pack",
+                        description = "Select up to " .. max_cards .. " cards from the opened pack (1-" .. total_cards .. ")",
+                        schema = {
+                            type = "object",
+                            properties = {
+                                card_indices = {
+                                    type = "array",
+                                    items = {
+                                        type = "integer",
+                                        minimum = 1,
+                                        maximum = total_cards
+                                    },
+                                    minItems = 1,
+                                    maxItems = max_cards,
+                                }
+                            },
+                            required = {"card_indices"}
+                        }
+                    },
+                    executor = select_from_pack_executor
+                }
+            end
         end
     end
 
